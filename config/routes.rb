288e580676b1c0ffc658ff9264fde928c597adae
huang <huangdey973@gmail.com>
2018-01-01 18:48:46 +0100

    Newinfo

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   end
 
   post 'cars/search', to: 'cars#search'
+  get 'pages/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
